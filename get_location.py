d6c3257b8c7796413aa38a46d97f825590dae6a6
YazanRihan <yazanrihan@gmail.com>
2023-10-20 18:39:33 +0400

    Get location initiated

---
+++ get_location.py
@@ -0,0 +1,15 @@
+from pymavlink import mavutil
+
+the_connection = mavutil.mavlink_connection('/dev/serial0', 57600)
+
+the_connection.wait_heartbeat()
+
+print("Heartbeat from system (system %u component %u)" % (the_connection.target_system, the_connection.target_component))
+
+try: 
+    altitude = the_connection.messages['GPS_RAW_INT'].alt  # Note, you can access message fields as attributes!
+    timestamp = the_connection.time_since('GPS_RAW_INT')
+except:
+    print('No GPS_RAW_INT message received')
+
+print(altitude)
