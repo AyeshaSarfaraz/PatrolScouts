4ce4717c72df24b9df6808b789609f933089f84c
YazanRihan <yazanrihan@gmail.com>
2023-10-26 17:53:07 +0400

    minor fixes

--- src/get_location.py
+++ src/get_location.py
@@ -1,4 +1,5 @@
 from pymavlink import mavutil
+from time import sleep
 
 the_connection = mavutil.mavlink_connection('/dev/serial0', 57600)
 
@@ -6,14 +7,19 @@ the_connection.wait_heartbeat()
 
 print("Heartbeat from system (system %u component %u)" % (the_connection.target_system, the_connection.target_component))
 
-try: 
-    altitude = the_connection.messages['GPS_RAW_INT'].alt # Note, you can access message fields as attributes!
-    longitude =  the_connection.messages['GPS_RAW_INT'].lon
-    latitude =  the_connection.messages['GPS_RAW_INT'].lat
-    timestamp = the_connection.time_since('GPS_RAW_INT')
-except:
-    print('No GPS_RAW_INT message received')
 
-print(altitude)
-print(longitude)
-print(latitude)
+
+while True:
+    sleep(1)
+    try:
+        altitude = the_connection.messages['GPS_RAW_INT'].alt # Note, you can access message fields as attributes!
+        longitude =  the_connection.messages['GPS_RAW_INT'].lon
+        latitude =  the_connection.messages['GPS_RAW_INT'].lat
+        timestamp = the_connection.time_since('GPS_RAW_INT')
+        print(altitude)
+        print(longitude)
+        print(latitude)
+    except:
+        print('No GPS_RAW_INT message received')
+
+
