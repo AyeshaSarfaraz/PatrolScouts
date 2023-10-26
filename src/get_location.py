b2294964a77fcda5a202d2a9abebbf21caa8dc62
YazanRihan <yazanrihan@gmail.com>
2023-10-26 17:19:22 +0400

    Added long and lat

--- src/get_location.py
+++ src/get_location.py
@@ -7,9 +7,13 @@ the_connection.wait_heartbeat()
 print("Heartbeat from system (system %u component %u)" % (the_connection.target_system, the_connection.target_component))
 
 try: 
-    altitude = the_connection.messages['GPS_RAW_INT'].alt  # Note, you can access message fields as attributes!
+    altitude = the_connection.messages['GPS_RAW_INT'].alt # Note, you can access message fields as attributes!
+    longitude =  the_connection.messages['GPS_RAW_INT'].lon
+    latitude =  the_connection.messages['GPS_RAW_INT'].lat
     timestamp = the_connection.time_since('GPS_RAW_INT')
 except:
     print('No GPS_RAW_INT message received')
 
 print(altitude)
+print(longitude)
+print(latitude)
