6c02ff3bfab25f2f078ddd1db0661011e366197d
YazanRihan <yazanrihan@gmail.com>
2023-10-26 18:28:17 +0400

    Verified get GPS location using pymavlink and dronekit

--- src/dronekit_get_location.py
+++ src/dronekit_get_location.py
@@ -1,5 +1,4 @@
 from dronekit import connect, VehicleMode
-import time
 
 connection_string = "/dev/serial0"
 baud_rate = 57600
@@ -11,3 +10,5 @@ vehicle.wait_ready('autopilot_version')
 print(f"Autopilot version: {vehicle.version}")
 
 print(f"Attitude is: {vehicle.attitude}")
+
+print(f"vehicle location: {vehicle.location.global_relative_frame}")
